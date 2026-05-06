b093d36f66d2415dbad2b69eede9315aff70a344
Ira Lisovska <lisovskaya_irina@list.ru>
2026-05-06 19:22:33 +0300

    Fix CartTest

--- src/test/java/CartTest.java
+++ src/test/java/CartTest.java
@@ -19,6 +19,9 @@ public class CartTest extends BaseTest {
     Ожидаемый результат: имя и стоимость товара идентичны как в сниппите карточки товара, так и в корзине
     */
 
+    String expectedProductName = "Sauce Labs Backpack";
+    String expectedPriceOfProduct = "29.99";
+
     @Test
     public void checkCart() {
         SoftAssert softAssert = new SoftAssert();
@@ -28,13 +31,10 @@ public class CartTest extends BaseTest {
         driver.findElement(By.className("submit-button")).click();
         driver.findElement(By.xpath("//*[@id='add-to-cart-sauce-labs-backpack']")).click();
         driver.findElement(By.xpath("//*[@id='shopping_cart_container']/a")).click();
-        String expectedProductName = "Sauce Labs Backpack";
-        String expectedPriceOfProduct = "29.99";
         String actualProductName = driver.findElement(By.xpath("//div[@class='inventory_item_name' and text()='Sauce Labs Backpack']")).getText();
         String actualPriceOfProduct = driver.findElement(By.xpath("//div[@class='cart_item' and .//div[text()='Sauce Labs Backpack']]//div[@class='inventory_item_price']")).getText().replace("$", "");
         softAssert.assertEquals(actualProductName, expectedProductName, "Название товара не совпадает.");
         softAssert.assertEquals(actualPriceOfProduct, expectedPriceOfProduct, "Стоимость товара не совпадает.");
         softAssert.assertAll();
-
     }
 }
