5c1230ab690f703ea5b3a546c660ed25c4f6d313
Ira Lisovska <lisovskaya_irina@list.ru>
2026-06-10 20:08:47 +0300

    Added addArguments options

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -44,11 +44,17 @@ public class BaseTest {
             options.addArguments("--disable-popup-blocking");
             options.addArguments("--disable-infobars");
             options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
             driver = new ChromeDriver(options);
             driver.manage().window().maximize();
         } else if (browser.equalsIgnoreCase("firefox")) {
             FirefoxOptions options = new FirefoxOptions();
             options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
             driver = new FirefoxDriver(options);
         }
 
